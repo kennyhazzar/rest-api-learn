1981b6c5befb7bca0f8957ce2bdb734b0ed4e9ae
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2021-10-27 20:58:09 +0300

    test

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,33 @@
+﻿package main
+
+import (
+	"flag"
+	"http-api/internal/app/apiserver"
+	"log"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+	// fmt.Println(configPath)
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
+
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
